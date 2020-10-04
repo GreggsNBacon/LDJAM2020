81955b2152df09ccf101404d2e8b7aece4758913
Maxime Vincent <34112203+MaxDaMiner@users.noreply.github.com>
2020-10-04 13:16:08 -0400

    all my stuff

---
+++ LDJAM2020/Assets/wheelRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelRotate : MonoBehaviour
+{
+    public GameObject frontWheelR;
+    public GameObject frontWheelL;
+
+    [SerializeField]
+    private float rotateSpeed = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotateSpeed, 0, 0) * Time.deltaTime * 20);
+
+    }
+}
