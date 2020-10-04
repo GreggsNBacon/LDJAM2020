4cc51f0703eec49c094a49dd5314444266f9f8d1
Gregory Neale <greg.neale@hotmail.com>
2020-10-04 16:43:49 +0100

    Update FOVSpeedController.cs

--- LDJAM2020/Assets/Scripts/Player/FOVSpeedController.cs
+++ LDJAM2020/Assets/Scripts/Player/FOVSpeedController.cs
@@ -14,6 +14,6 @@ public class FOVSpeedController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        fovController.SetCameraFOV(0.5f);
+        fovController.SetCameraFOV(controller.GetSpeed());
     }
 }
