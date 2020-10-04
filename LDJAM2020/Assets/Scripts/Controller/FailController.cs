69b8bc291865cc3cc32f3bcd3c73208090210c95
Gregory Neale <greg.neale@hotmail.com>
2020-10-04 23:26:47 +0100

    Fail Condition Event

---
+++ LDJAM2020/Assets/Scripts/Controller/FailController.cs
@@ -0,0 +1,17 @@
+﻿using LudumDare.Controller;
+using LudumDare.Core.EventManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailController : AbstractController
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            EventManager<Events>.TriggerEvent(Events.FailConditionMet);
+            Debug.Log("FAIL CONDITION MET");
+        }
+    }
+}
