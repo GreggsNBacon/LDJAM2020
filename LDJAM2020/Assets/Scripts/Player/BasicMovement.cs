67932104812990f8a5fd38d0fe3a0712f39866c1
WilliamAkins <w.akins111@gmail.com>
2020-10-04 01:43:59 +0100

    almost fixed movement

--- LDJAM2020/Assets/Scripts/Player/BasicMovement.cs
+++ LDJAM2020/Assets/Scripts/Player/BasicMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    [SerializeField]
     private float minSpeed = 1.0f;
     private float boost = 1.0f;
     private float desiredValue = 0.0f;
@@ -16,16 +17,87 @@ public class BasicMovement : MonoBehaviour
     private float rotSpeed = 10.0f;
     private float turning = 0.0f;
 
+    [SerializeField] private LayerMask mask;
+
+    Quaternion desiredRot = Quaternion.identity;
+
 
     // Update is called once per frame
     void Update()
     {
-        UpdateThrottle();
-        transform.position += transform.forward * (minSpeed * Time.deltaTime);
-        transform.position += transform.forward * (boost * maxBoost * Time.deltaTime);
-        transform.position += transform.right * (turning * Time.deltaTime);
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (!Physics.Raycast(ray, out var hit, 100, mask, QueryTriggerInteraction.Ignore))
+        {
+            return;
+        }
 
-       // transform.position += transform.forward;
+        // Just in case, also make sure the collider also has a renderer
+        // material and texture
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+        if (meshCollider == null || meshCollider.sharedMesh == null)
+        {
+            return;
+        }
+
+        Mesh mesh = meshCollider.sharedMesh;
+        Vector3[] normals = mesh.normals;
+        int[] triangles = mesh.triangles;
+
+        // Extract local space normals of the triangle we hit
+        Vector3 n0 = normals[triangles[hit.triangleIndex * 3 + 0]];
+        Vector3 n1 = normals[triangles[hit.triangleIndex * 3 + 1]];
+        Vector3 n2 = normals[triangles[hit.triangleIndex * 3 + 2]];
+
+        // interpolate using the barycentric coordinate of the hitpoint
+        Vector3 baryCenter = hit.barycentricCoordinate;
+
+        // Use barycentric coordinate to interpolate normal
+        Vector3 interpolatedNormal = n0 * baryCenter.x + n1 * baryCenter.y + n2 * baryCenter.z;
+        // normalize the interpolated normal
+        interpolatedNormal = interpolatedNormal.normalized;
+
+        // Transform local space normals to world space
+        Transform hitTransform = hit.collider.transform;
+        interpolatedNormal = hitTransform.TransformDirection(interpolatedNormal);
+
+        // Display with Debug.DrawLine
+        Debug.DrawRay(hit.point, interpolatedNormal);
+
+        // Display with Debug.DrawLine
+        Debug.DrawRay(hit.point, hit.normal, Color.red);
+
+        desiredRot = Quaternion.FromToRotation(transform.up, interpolatedNormal) * transform.rotation;
+        //carTransform.rotation = desiredRot;
+
+        //transform.rotation = desiredRot;
+        transform.rotation = desiredRot;
+        //transform.up = interpolatedNormal;
+
+        UpdateThrottle();
+
+        //var localRotation = transform.localRotation;
+        //localRotation = Quaternion.Euler(localRotation.x, localRotation.y + turning * rotSpeed * Time.deltaTime, localRotation.z);
+        //transform.localRotation = localRotation;
+
+        if (turning != 0)
+        {
+            transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + turning * rotSpeed * Time.deltaTime, transform.rotation.z));
+        }
+
+        transform.localPosition += transform.forward * (minSpeed * Time.deltaTime);
+
+        //transform.localRotation = Quaternion.Euler(0.0f, (turning * Time.deltaTime), 0.0f) * transform.localRotation;
+
+        //transform.position += transform.forward * (boost * maxBoost * Time.deltaTime);
+        //transform.position += transform.right * (turning * Time.deltaTime);
+
+        //transform.Translate(Vector3.forward * (minSpeed * Time.deltaTime));
+
+        //transform.Rotate(transform.right * (turning * Time.deltaTime));
+
+        //transform.up = interpolatedNormal;
+
+        // transform.position += transform.forward;
     }
 
     void UpdateThrottle()
