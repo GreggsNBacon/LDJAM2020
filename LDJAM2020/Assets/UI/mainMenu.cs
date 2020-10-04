91d9bbb5aaa30085cb1e4fb45b41297e1c8d8d7d
Gregory Neale <greg.neale@hotmail.com>
2020-10-05 00:20:50 +0100

    re-removing the tutorial stuff, but keeping the lap collider

--- LDJAM2020/Assets/UI/mainMenu.cs
+++ LDJAM2020/Assets/UI/mainMenu.cs
@@ -7,7 +7,7 @@ public class mainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("LevelScene");
     }
 
     public void quitGame()
