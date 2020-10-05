e38a2f86d91f31862519a973bb093cce5f434720
WilliamAkins <w.akins111@gmail.com>
2020-10-05 22:51:30 +0100

    finish

---
+++ LDJAM2020/Assets/Scripts/Player/FollowWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class FollowWall : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target = null;
+
+    [SerializeField]
+    private float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dtSpeed = Time.deltaTime * speed;
+        transform.position = Vector3.Lerp(transform.position, target.position, dtSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(target.rotation.x, target.rotation.y, target.rotation.z, target.rotation.w), dtSpeed);
+    }
+}
